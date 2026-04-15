e2d6db438b282afae2121ecfef71939594bacd18
ValeryMTribalesG <valerymtg@gmail.com>
2026-04-15 18:44:15 -0500

    Cambios controller

---
+++ reservas/reservas/src/main/java/com/turismo/reservas/controller.java
@@ -0,0 +1,28 @@
+package com.turismo.reservas.controller;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import com.turismo.reservas.model.Reserva;
+import com.turismo.reservas.service.ReservaService;
+
+@RestController
+@RequestMapping("/reservas")
+@CrossOrigin
+public class ReservaController {
+
+    private final ReservaService service;
+
+    public ReservaController(ReservaService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Reserva> listar() {
+        return service.listar();
+    }
+
+    @PostMapping
+    public Reserva guardar(@RequestBody Reserva reserva) {
+        return service.guardar(reserva);
+    }
+}
---
+++ reservas/reservas/src/main/java/com/turismo/reservas/services.java
@@ -0,0 +1,24 @@
+package com.turismo.reservas.service;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.turismo.reservas.model.Reserva;
+import com.turismo.reservas.repository.ReservaRepository;
+
+@Service
+public class ReservaService {
+
+    private final ReservaRepository repository;
+
+    public ReservaService(ReservaRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Reserva> listar() {
+        return repository.listar();
+    }
+
+    public Reserva guardar(Reserva reserva) {
+        return repository.guardar(reserva);
+    }
+}
