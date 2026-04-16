d5ad72201621a190290abda258e41d43c70352a5
ValeryMTribalesG <valerymtg@gmail.com>
2026-04-15 19:04:29 -0500

    Cambios

--- reservas/reservas/src/main/java/com/turismo/reservas/repositoryaInterface.java
+++ reservas/reservas/src/main/java/com/turismo/reservas/repositoryaInterface.java
@@ -1,9 +1,9 @@
 package com.turismo.reservas.repository;
 
-import java.util.List;
 import com.turismo.reservas.model.Reserva;
 
 public interface ReservaRepository {
-    List<Reserva> listar();
+
+    Reserva[] listar();
     Reserva guardar(Reserva reserva);
 }
